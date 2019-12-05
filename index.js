3a27a0bcfc06fb7164037139c93059727a32aa3f
Unnikrishnnan R <unni.iceman@gmail.com>
2019-12-05 15:39:23 +0530

    Aded missing blockly media files, added more blocks (pen, logic, math etc), implemented features to ensure single instances of specific blocks, code can be transmitted to HaathiMeraSaathi_PyBlockly unity project, added devtools to interface/.

--- index.js
+++ index.js
@@ -1,22 +1,40 @@
-// Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
+const { app, BrowserWindow } = require('electron')
 
 //From https://gist.github.com/tedmiston/5935757
 var net = require('net');
+var isSocketConnected = false;
 
-var server = net.createServer(function(socket) {
-	socket.write('Echo server Unni!\r\n');
-	//socket.pipe(socket);
+var socket = net.createConnection(1337, '127.0.0.1');
+console.log('Socket created.');
+socket.on('data', function(data) {
+  // Log the response from the HTTP server.
+  console.log('RESPONSE: ' + data);
+});
+socket.on('connect', function() {
+  // Manually write an HTTP request.
+  isSocketConnected = true;
+  //socket.write("Connected to PyBlockly");
+});
+socket.on('end', function() {
+  isSocketConnected = false;
+  console.log('Disconnected');
+});
+socket.on('error', function() {
+  console.log('Connection error!');
+});
+
+/*var server = net.createServer(function(socket) {
+  socket.write('Echo server Unni!\r\n');
+  //socket.pipe(socket);
 
   socket.on('connect', function(data) {
-  	console.log('Connected with client');
-  	//client.destroy(); // kill client after server's response
+    console.log('Connected with client');
+    //client.destroy(); // kill client after server's response
   });
 
   socket.on('data', function(data) {
-  	console.log('Received data from client : ' + data);
-  	//client.destroy(); // kill client after server's response
+    console.log('Received data from client : ' + data);
+    //client.destroy(); // kill client after server's response
   });
 
   socket.on('close', function(data) {
@@ -26,44 +44,54 @@ var server = net.createServer(function(socket) {
 
 });
 
-server.listen(1337, '127.0.0.1');
-
-
+server.listen(1337, '127.0.0.1');*/
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-let mainWindow
+let win
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({
-    width: 1280,
+  win = new BrowserWindow({
+    width: 1680,
     height: 720,
-		frame: true,
-		title: "PyBlockly",
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+      nodeIntegration: true
+    },
+    frame:true
   })
 
-	mainWindow.setMenuBarVisibility(false);
-  //require('./server')
-  //require('./client')
-
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  win.loadFile('test.html')
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  win.webContents.openDevTools();
+
+  win.autoHideMenuBar = true;
 
   // Emitted when the window is closed.
-  mainWindow.on('closed', function () {
+  win.on('closed', () => {
     // Dereference the window object, usually you would store windows
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
-    mainWindow = null
+    win = null
   })
 }
+
+const { ipcMain } = require('electron')
+ipcMain.on('asynchronous-message', (event, arg) => {
+  console.log(arg) // prints "ping"
+  if(isSocketConnected)
+  {
+    socket.write(arg);
+  }
+  event.reply('asynchronous-reply', 'pong')
+})
+
+ipcMain.on('synchronous-message', (event, arg) => {
+  console.log(arg) // prints "ping"
+  event.returnValue = 'pong'
+})
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
@@ -71,17 +99,18 @@ function createWindow () {
 app.on('ready', createWindow)
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
 })
 
-app.on('activate', function () {
+app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (mainWindow === null) createWindow()
+  if (win === null) {
+    createWindow()
+  }
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
